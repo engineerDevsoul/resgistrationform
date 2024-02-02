a1baf86e1825eba5fb7635dc7e8b0609b6dc1192
engineerDevsoul <154658564+engineerDevsoul@users.noreply.github.com>
2024-02-02 20:11:04 +0530

    added index.html

---
+++ index.js
@@ -0,0 +1,102 @@
+var express=require("express")
+var bodyparser=require("body-parser")
+var mongoose=require("mongoose")
+const bodyParser = require("body-parser")
+
+const app=express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+mongoose.connect('mongodb://localhost:27017/Database')
+var db=mongoose.connection
+db.on('error',()=> console.log("Error in onnecting to database"))
+db.once('open',()=> console.log("connection to Database"))
+
+app.post("/sign_up",(req,res)=>
+{
+    var name=req.body.name
+    var age=req.body.age
+    var email=req.body.email
+    var phone=req.body.phone
+    var gender=req.body.gender
+    var password=req.body.password
+
+    var data ={
+        "name":name,
+        "age":age,
+        "phone":phone,
+        "gender":gender,
+        "password":password
+    }
+    db.collection('users').insertOne(data,(err,collection) =>{
+         if(err){
+                throw err;
+        }
+        console.log("Record Inserted Succesfully")
+    })
+    return res.redirect('signup_succesful.html')
+
+
+})
+
+app.get("/",(req,res) => {
+    res.set({"Allow-acces-Allow-Origin":'*'
+})
+return res.redirect('index.html')
+
+}).listen(3005);
+
+console.log("listening on port 3000")
+// var express=require("express")
+// var bodyParser=require("body-parser")
+// var mongoose=require("mongoose")
+
+// const app=express()
+
+// app.use(bodyParser.json())
+// app.use(express.static('public'))
+// app.use(bodyParser.urlencoded({
+//     extended:true
+// }))
+
+// mongoose.connect('mongodb://localhost:27017/Database')
+// var db=mongoose.connection
+// db.on('error',()=> console.log("Error in Connecting to Database"))
+// db.once('open',()=> console.log("Connected to Database"))
+
+// app.post("/sign_up",(req,res) => {
+//     var name= req.body.name
+//     var age=req.body.age
+//     var email=req.body.email
+//     var phno=req.body.phno
+//     var gender=req.body.gender
+//     var password=req.body.password
+
+//     var data={
+//         "name":name,
+//         "age":age,
+//         "email":email,
+//         "phno":phno,
+//         "gender":gender,
+//         "password":password
+//     }
+//     db.collection('users').insertOne(data,(err,collection) => {
+//         if(err){
+//             throw err;
+//         }
+//         console.log("Record Inserted Succesfully")
+//     })
+//     return res.redirect('signup_successful.html')
+// })
+
+// app.get("/",(req,res) => {
+//     res.set({
+//         "Allow-acces-Allow-Origin":'*'
+//     })
+//     return res.redirect('index.html')
+// }).listen(3005);
+
+// console.log("Listening on port 3000")
